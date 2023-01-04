4ba1cc6dcd9affd2daa9bf45d0902c25861415e9
caiolaytynher <caiolaytynherbr@gmail.com>
2023-01-04 20:32:29 -0300

    added delete script

---
+++ delete-gtk-themes.fish
@@ -0,0 +1,26 @@
+#!/bin/fish
+
+# This script should be run with root privileges and with the argument being
+# the home folder
+
+set -l home $argv[1]
+if not test -e $home
+  exit
+end
+
+set -l themes_repo 'Everforest-GTK-Theme' 'Gruvbox-GTK-Theme' 'Kanagawa-GKT-Theme' 'Tokyo-Night-GTK-Theme'
+set -l themes 'Everforest-Dark-BL' 'Gruvbox-Dark-BL' 'Kanagawa-BL' 'Tokyonight-Dark-BL'
+set -l themes_path $home/Programs
+set -l gtk_themes_path /usr/share/themes
+
+for theme in $themes_repo
+  if test -e $themes_path/$theme
+    command rm -rf $themes_path/$theme
+  end
+end
+
+for theme in $themes
+  if test -e $gtk_themes_path/$theme
+    command rm -r $themes_path/$theme $gtk_themes_path
+  end
+end
