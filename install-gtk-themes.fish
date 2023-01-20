7c30292307188563291712dd42a49bae83e1822e
caiolaytynher <caiolaytynherbr@gmail.com>
2023-01-20 17:15:12 -0300

    removed git actions

--- install-gtk-themes.fish
+++ install-gtk-themes.fish
@@ -1,7 +1,8 @@
 #!/bin/fish
 
 # This script should be run with root privileges and with the argument being
-# the home folder
+# the home folder. We need root privileges because the gtk themes folder is
+# protected.
 
 # Check if home folder exists
 set -l home $argv[1]
@@ -9,28 +10,18 @@ if not test -e $home
   exit
 end
 
+# Check for the themes folder
+if not test -e $home/Themes
+  echo 'Please, clone the custom-themes repo to the Themes folder in your home folder.'
+  exit
+end
+
 set -l themes_repo 'Everforest-GTK-Theme' 'Gruvbox-GTK-Theme' 'Kanagawa-GKT-Theme' 'Tokyo-Night-GTK-Theme'
 set -l themes 'Everforest-Dark-BL' 'Gruvbox-Dark-BL' 'Kanagawa-BL' 'Tokyonight-Dark-BL'
-set -l themes_path $home/Programs
-set -l gtk_themes_path /usr/share/themes
-
-# Create Programs folder if none
-if not test -e $home/Programs
-  command mkdir $home/Programs
-end
-
-# Clone or update repos
-for theme in $themes_repo
-  if test -e $themes_path/$theme
-    command git -C $themes_path/$theme pull
-  else
-    command git -C $themes_path clone https://github.com/Fausto-Korpsvart/$theme.git
-  end
-end
 
 # Copy themes to gtk themes folder
-for theme in $themes
-  if not test -e $gtk_themes_path/$theme
-    command cp -r $themes_path/$theme $gtk_themes_path
+for i in (seq 1 4)
+  if not test -e /usr/share/themes/$themes[i]
+    command cp -r $home/Themes/$themes_repo[i]/themes/$themes[i] /usr/share/themes
   end
 end
