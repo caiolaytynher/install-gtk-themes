3fe0631acf1f6ab3407bfba90af2f41fc1297499
caiolaytynher <caiolaytynherbr@gmail.com>
2023-01-20 17:15:36 -0300

    removed deleting the git repos

--- delete-gtk-themes.fish
+++ delete-gtk-themes.fish
@@ -1,7 +1,7 @@
 #!/bin/fish
 
 # This script should be run with root privileges and with the argument being
-# the home folder
+# the home folder. It needs privileges because the themes folder is protected.
 
 # Check if home folder exists
 set -l home $argv[1]
@@ -9,21 +9,13 @@ if not test -e $home
   exit
 end
 
-set -l themes_repo 'Everforest-GTK-Theme' 'Gruvbox-GTK-Theme' 'Kanagawa-GKT-Theme' 'Tokyo-Night-GTK-Theme'
 set -l themes 'Everforest-Dark-BL' 'Gruvbox-Dark-BL' 'Kanagawa-BL' 'Tokyonight-Dark-BL'
 set -l themes_path $home/Programs
 set -l gtk_themes_path /usr/share/themes
 
-# Delete repos
-for theme in $themes_repo
-  if test -e $themes_path/$theme
-    command rm -rf $themes_path/$theme
-  end
-end
-
 # Delete themes
 for theme in $themes
-  if test -e $gtk_themes_path/$theme
-    command rm -r $themes_path/$theme $gtk_themes_path
+  if test -e /usr/share/themes/$theme
+    command rm -r /usr/share/themes/$theme
   end
 end
